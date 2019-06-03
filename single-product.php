4451bbd59db592f55e7fac5c26152663fa324b99
f16hari <aeromasterhari15@gmail.com>
2019-06-03 12:07:10 +0530

    added modal in sigleproduct.php"

--- single-product.php
+++ single-product.php
@@ -69,13 +69,13 @@ $row = $result->fetch_assoc();
 			<div class="row s_product_inner">
 				<div class="col-lg-6">
 					<div class="s_Product_carousel">
-						<div class="single-prd-item">
+						<div class="single-prd-item" id="myBtn">
 							<img class="img-fluid" src="product_images/<?php echo $row['product_id']?>.jpg" alt="">
 						</div>
-						<div class="single-prd-item">
+						<div class="single-prd-item" id="myBtn">
 							<img class="img-fluid" src="img/category/s-p1.jpg" alt="">
 						</div>
-						<div class="single-prd-item">
+						<div class="single-prd-item" id="myBtn">
 							<img class="img-fluid" src="img/category/s-p1.jpg" alt="">
 						</div>
 					</div>
@@ -107,6 +107,16 @@ $row = $result->fetch_assoc();
 			</div>
 		</div>
 	</div>
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+		<span class="close">&times;</span>
+		</div>
+
+    </div>
 	<!--================End Single Product Area =================-->
 
 	<!--================Product Description Area =================-->
@@ -312,6 +322,33 @@ $row = $result->fetch_assoc();
 	<!-- start footer Area -->
 	<?php include("footer.php");?>
 	<!-- End footer Area -->
+
+	<script>	
+		var modal = document.getElementById("myModal");
+
+		// Get the button that opens the modal
+		var btn = document.getElementById("myBtn");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks on the button, open the modal 
+		btn.onclick = function() {
+		modal.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		modal.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+		}
+	</script>
 
 	<script src="js/vendor/jquery-2.2.4.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
